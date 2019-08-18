ebe9a0a5f3919ac0fb7ec8914aa5989adee51717
anton <admin@btcontract.com>
2019-08-18 15:01:07 +0300

    Minor fix

--- app/src/main/java/com/lightning/walletapp/AbstractKit.java
+++ app/src/main/java/com/lightning/walletapp/AbstractKit.java
@@ -23,7 +23,6 @@ public abstract class AbstractKit extends AbstractIdleService {
     public static final String RATES_DATA = "ratesData";
     public static final String FIAT_TYPE = "fiatType1";
 
-
     // Bitcoin wallet core pieces
     public volatile BlockChain blockChain;
     public volatile PeerGroup peerGroup;
