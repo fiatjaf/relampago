378252672a2b9e9ce79e4d7dc6be0163e6b3182a
anton <admin@btcontract.com>
2019-09-02 10:02:22 +0300

    Revert "Improve node alias handling"

--- app/src/main/java/com/lightning/walletapp/ln/wire/LightningMessage.scala
+++ app/src/main/java/com/lightning/walletapp/ln/wire/LightningMessage.scala
@@ -141,8 +141,7 @@ case class NodeAnnouncement(signature: ByteVector, features: ByteVector, timesta
   } getOrElse "No IP address"
 
   val identifier = (alias + nodeId.toString).toLowerCase
-  val trimmedAlias = alias.take(18).takeWhile(_ != '\u0020')
-  val asString = s"<strong>$trimmedAlias</strong><br><small>$pretty</small>"
+  val asString = s"<strong>${alias.takeWhile(_ != '\u0020') take 18}</strong><br><small>$pretty</small>"
   lazy val hostedChanId = Tools.hostedChanId(LNParams.nodePublicKey.toBin, nodeId.toBin)
 }
 
